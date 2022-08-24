47c57d7f34c2bec225fec3494dbe26ed2ff90df2
sasori35000 <102420746+sasori35000@users.noreply.github.com>
2022-08-24 20:03:44 +0200

    Add files via upload

---
+++ Script_Essaie.js
@@ -0,0 +1,44 @@
+function myFunction1() {
+  document.getElementById("change").style.backgroundColor = "#66ff99";
+  document.getElementById("change").style.color = "white";
+  document.getElementById("change2").style.backgroundColor = "#66ff99";
+  document.getElementById("change2").style.color = "white";
+}
+function myFunction2() {
+  document.getElementById("change").style.backgroundColor = "rgba(255, 255, 255, .0)";
+  document.getElementById("change").style.color = "black";
+  document.getElementById("change2").style.backgroundColor = "rgba(255, 255, 255, .0)";
+  document.getElementById("change2").style.color = "black";
+}
+function myFunction3() {
+	if (document.getElementById("pop1").src = "img1.jpg") {
+  document.getElementById("pop1").src = "img2.jpg";
+  document.getElementById("pop2").src = "img3.jpg";
+  document.getElementById("pop3").src = "img1.jpg";
+	} 
+}
+function myFunction4() {
+   if (document.getElementById("pop1").src = "img2.jpg") {
+  document.getElementById("pop1").src = "img3.jpg";
+  document.getElementById("pop2").src = "img1.jpg";
+  document.getElementById("pop3").src = "img2.jpg";
+	}
+}
+function myFunction5() {
+  document.getElementById("1").style.borderColor = "blue";
+}
+function myFunction6() {
+  document.getElementById("1").style.borderColor = "rgba(255, 255, 255, .0)";
+}
+function myFunction7() {
+  document.getElementById("2").style.borderColor = "blue";
+}
+function myFunction8() {
+  document.getElementById("2").style.borderColor = "rgba(255, 255, 255, .0)";
+}
+function myFunction9() {
+  document.getElementById("3").style.borderColor = "blue";
+}
+function myFunction10() {
+  document.getElementById("3").style.borderColor = "rgba(255, 255, 255, .0)";
+}
